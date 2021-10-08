99affed7962dd41fe70dac385e4f61d11971a8b0
Kylene <kylenehohman@gmail.com>
2021-10-08 02:11:47 -0400

    latest update

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
-//js edit
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector('body');
+
+    let x = -1; 
+    const images = [];
+    images[0] = "assets/stage1.jpg"
+    images[1] = "assets/stage2.jpg"
+    images[2] = "assets/stage3.jpg"
+    images[3] = "assets/stage4.jpg"
+
+    function plantDying () {
+        x = (x === images.length - 1) ? 0 : x + 1;
+        document.getElementById('img').src = images[x];
+    }
+
+    function plantHealing () {
+        x = (x <= 0) ? images.length -1 : x - 1;
+        document.getElementById('img').src = images[x];
+    }
+
+    function startTime () {
+        setInterval(plantDying, 4000);
+    }
+
+    startTime(); 
+
+
+
+
+    dragElement(document.getElementById("mydiv"));
+
+    function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        //check if watering can collides with plant 
+        console.log(elmt.style.top); 
+        if (elmnt.style.top === "0px" && elmnt.style.left === "0px"){
+            let audio = new Audio ('assets/water.mp3'); 
+            audio.play(); 
+        }
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+    }
+
+ 
+
+
+
+
+
+
+
+
+
+})
